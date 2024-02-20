0988d73d0be68ae36c488ec7d3ac8dec36078818
Aman Agnihotri <69078309+amanagnihotri1@users.noreply.github.com>
2024-02-20 12:18:05 +0530

    Update Social.jsx

--- src/component/Home/Social.jsx
+++ src/component/Home/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export const Social = () => {
   return (
    <div className='home__social'>
-    <a href='https://twitter.com/XEGOatyoutube' className='home__social-icon' target='_blank'>
+    <a href="https://twitter.com/XEGOatyoutube" className='home__social-icon' target='_blank'>
     <i class="uil uil-twitter-alt"></i>
     </a>
-    <a href='https://github.com/amanagnihotri1' className='home__social-icon' target='_blank'>
+    <a href="https://github.com/amanagnihotri1" className='home__social-icon' target='_blank'>
     <i class="uil uil-github-alt"></i>
     </a>
-    <a href='https://www.linkedin.com/in/aman-agnihotri-818102192/' className='home__social-icon' target='_blank'>
+    <a href="https://www.linkedin.com/in/aman-agnihotri-818102192/" className='home__social-icon' target='_blank'>
     <i class="uil uil-linkedin-alt"></i>
     </a>
    </div> 
